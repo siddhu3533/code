dcdcf13d91829bad9b1777c8c71145d4531d359a
siddhu3533 <siddhu35333@gmail.com>
2016-10-22 20:34:48 +0530

    First repeated one

---
+++ first.py
@@ -0,0 +1,17 @@
+from collections import Counter
+
+values = []
+
+n = input()
+n = int(n)
+counterVar = 0
+tempVar = ""
+for counterVar in range(n):
+    tempVar = input()
+    values.append(tempVar)
+countValues = Counter(values)
+
+for key,value in countValues.items():
+    if value >=2:
+        print(key)
+        break    
